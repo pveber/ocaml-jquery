51fe79d70a8c0bdb2edfcf2b0f33f20d512f8b0b
Gabriel Cardoso <gcardoso.w@gmail.com>
2011-08-23 12:24:09 +0200

    Changed array to js_array in jQuery function

--- core.ml
+++ core.ml
@@ -14,7 +14,7 @@ class type jQuery = object
   method css : js_string t -> js_string t -> unit meth
 end
 
-let jQuery : (js_string t, Dom_html.element t, Dom_html.element t array t,
+let jQuery : (js_string t, Dom_html.element t, Dom_html.element t js_array t,
 	      jQuery t) Tools.Choice4.t ->
               (Dom_html.element t, Dom_html.document, 
 	       jQuery t) Tools.Choice3.t opt -> jQuery t = 
